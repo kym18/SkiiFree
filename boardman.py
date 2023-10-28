7d5dbf62f1b1688e40bf72433508ace1509a6d1c
kym18 <cooiyoo@naver.com>
2023-10-28 19:01:31 +0900

    보드타는 사람 추가

---
+++ boardman.py
@@ -0,0 +1,17 @@
+from pico2d import load_image
+import random
+
+class boardMan:
+    def __init__(self): #생성자 함수, 객체 생성될 때 맨 처음 자동 호출 -> 객체 초기 상태
+        self.image = load_image('boardman.png')
+        self.frame = 0
+        self.x, self.y = random.randint(50, 750), 820
+        self.speed = 5
+
+    def draw(self):
+            self.image.clip_draw(self.frame * 70 + 4 , 0, 73, 100, self.x, self.y, 50, 60)
+
+    def update(self):
+        self.frame = (self.frame + 1) % 3
+        print(self.frame)
+        self.y -= self.speed
