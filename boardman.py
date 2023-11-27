eaebcb25b7a09f99c4ea8309182631d263aa0811
kym18 <cooiyoo@naver.com>
2023-11-27 20:35:46 +0900

    보드맨 25초 주기로 재등장!!

--- boardman.py
+++ boardman.py
@@ -1,16 +1,23 @@
-from pico2d import load_image
+from pico2d import load_image, get_time
 import random
 
+
 class boardMan:
-    def __init__(self): #생성자 함수, 객체 생성될 때 맨 처음 자동 호출 -> 객체 초기 상태
+    def __init__(self):
         self.image = load_image('Images/boardman.png')
         self.frame = 0
         self.x, self.y = random.randint(50, 750), 820
         self.speed = 5
+        self.last_time = get_time()
 
     def draw(self):
-            self.image.clip_draw(self.frame * 70 + 4 , 0, 73, 100, self.x, self.y, 50, 60)
+        self.image.clip_draw(self.frame * 70 + 4, 0, 73, 100, self.x, self.y, 50, 60)
 
     def update(self):
-        self.frame = (self.frame + 1) % 3
-        self.y -= self.speed
+        current_time = get_time()
+        if current_time - self.last_time > 25.0:  # 25초마다
+            self.y = 800
+            self.last_time = current_time
+        else:
+            self.frame = (self.frame + 1) % 3
+            self.y -= self.speed
