70cf7254a06d269841efb5c87650dafdf0ff6b34
kym18 <cooiyoo@naver.com>
2023-11-27 20:25:35 +0900

    화면 하강구현-시작피켓

--- background.py
+++ background.py
@@ -19,9 +19,12 @@ class Snow:
 class Start:
     def __init__(self):  # 생성자 함수, 객체 생성될 때 맨 처음 자동 호출 -> 객체 초기 상태
         self.image = load_image('Images/start.png')
+        self.screen_height = 800  # 화면 높이 설정
+        self.y = Snow_HEIGHT - 80
 
     def draw(self):
-        self.image.draw(Snow_WIDTH // 2 - 50, Snow_HEIGHT - 80, 80, 50)
+        if self.y < 800:
+            self.image.draw(Snow_WIDTH // 2 - 50,self.y, 80, 50)
 
     def update(self):
-        pass
+        self.y += 10
