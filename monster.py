73a8b167613885a1f28724036489919fed841632
kym18 <cooiyoo@naver.com>
2023-11-30 19:10:22 +0900

    몬스터가 주인공 추적

--- monster.py
+++ monster.py
@@ -1,5 +1,8 @@
+import math
+
 from pico2d import load_image, get_time, draw_rectangle
 import random
+import play_mode
 
 
 class Monster:
@@ -16,7 +19,19 @@ class Monster:
         if self.action == 0:
             self.image.clip_draw(self.frame * 80 + 30, 0, 76, 130, self.x, self.y, 50, 60)
         elif self.action == 1:
-            self.image.clip_draw(self.frame * 70 + 78 * 3 - 12, 0, 80, 100, self.x, self.y, 40, 55)
+            self.image.clip_draw(self.frame * 80 + 30, 0, 76, 130, self.x, self.y, 50, 60)
+
+    def move_slightly_to(self, tx, ty):  # 조금씩 움직이는거
+        self.dir = math.atan2(ty - self.y, tx - self.x)
+        # self.speed = RUN_SPEED_PPS
+        self.x += self.speed * math.cos(self.dir)
+        self.y += self.speed * math.sin(self.dir)
+
+    def move_away(self, tx, ty):  # 조금씩 움직이는거
+        self.dir = math.atan2(ty - self.y, tx - self.x)
+        # self.speed = RUN_SPEED_PPS
+        self.x += self.speed * math.cos(self.dir)
+        self.y += self.speed * math.sin(self.dir)
 
     def update(self):
         current_time = get_time()
@@ -24,12 +39,14 @@ class Monster:
             self.y = 810  # 새로운 y 좌표 설정
             self.last_time = current_time
         else:
-            if self.action == 0:
+            if self.action == 0: #달려감
                 self.frame = (self.frame + 1) % 4
-            elif self.action == 1: #넘어짐
-                pass
+                self.move_slightly_to(play_mode.man.x, play_mode.man.y)
 
-            self.y -= self.speed
+            elif self.action == 1: #멀어딤
+                self.frame = (self.frame + 1) % 4
+                self.move_slightly_to(-play_mode.man.x, -play_mode.man.y)
+
 
     def get_bb(self):
         return self.x - 10, self.y - 20, self.x + 10, self.y + 20
