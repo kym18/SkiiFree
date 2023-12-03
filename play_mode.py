8a3bdc3db11a1fb59c188776f533c0fd9dec413b
kym18 <cooiyoo@naver.com>
2023-12-03 23:58:35 +0900

    handle_event 수정
    실행종료 수정

--- play_mode.py
+++ play_mode.py
@@ -15,9 +15,9 @@ def handle_events():
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            game_framework.quit()
+            running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            game_framework.quit()
+            running = False
             #game_framework.change_mode(explanation_mode)
         else:
             man.handle_event(event)
